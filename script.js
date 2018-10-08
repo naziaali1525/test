d386428a57cd1211712901e9d314c440d49db5fd
naziaali1525 <38943101+naziaali1525@users.noreply.github.com>
2018-10-08 00:48:05 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,208 +1,28 @@
 // Write all your Javascript here!
 function initialize(){
 
-    let bodyEl = document.querySelector("#container ");
+    let bodyEl = document.querySelector(".card ");
          
     $.ajax({
         url:`https://techkaro-test.herokuapp.com/api/v1/getdata`,
      success: function(data){
         console.log(data);
       
-        
-     document.querySelector("#container").innerHTML = 
-           `  <div id = "main-div">
-                    <h2>A Flower Day...</h2>
-                        <button type="button" class="btn btn-success"><em>what's new at TechKaro?</em></button>
-                    <div class="card" style="width: 500px" "height: 400px" >
-                    <img class="card-img-top" src="${data.imageURL}" alt="Card image cap">
-                    <div class="card-body card-date">
-                        <p class="date"><i class="fas fa-calendar-alt"></i>${data.date}</p>
-                        <p class="community"><i class="fas fa-user-alt"></i>by </p>
+               document.querySelector(".card").innerHTML = `<div class="card">
+                    <div class="card-header">
+                    <p class="date">${data.date}</p>
+                    <p class="techkaro">by TechKaro community</p>
                     </div>
+                    <div class="card-body">
+                    <h5 class="card-title">${data.info.heading}</h5>
+                    <p class="card-text">${data.info.description}</p>
                     </div>
-                    
-                    <div class="card" style="width: 500px;">
-                            <div class="card-body ">
-                                <h5 class="card-title ">${data.info.heading}</h5>
-                            
-                                <p class="card-text">${data.info.description}</p>
-                                
-                    </div>
-         </div>
-         </div>
-       
-       <div id = sale-div>
-         <div class= "big-sale">
-              <button type="button" class="btn btn-success"><em>New to TechKaro? <em>Learn More ></em></em></button>
-            <div class="card" style="width: 18rem;">
-               <div class="card-body">
-                 <h5 class="card-title">${data.sale.heading}</h5>
-                 <p class="card-text">${data.sale.description}</p>
-               </div> 
-           </div>
-        </div>
-        </div> `
+                </div>`
 
-        document.querySelector(".nav").innerHTML = data.menuOption[0];
-               
-               
+        
+
      }
     
      });
-
-
     
 }
-function getUr(){
-    $.ajax({
-        url:` https://techkaro-test.herokuapp.com/api/v1/getdata?lang=ur`,
- success: function(data){
-        console.log(data);
-      
-        
-     document.querySelector("#container").innerHTML = 
-           `  <div id = "main-div">
-                    <h2>A Flower Day...</h2>
-                        <button type="button" class="btn btn-success"><em>what's new at TechKaro?</em></button>
-                    <div class="card" style="width: 500px" "height: 400px" >
-                    <img class="card-img-top" src="${data.imageURL}" alt="Card image cap">
-                    <div class="card-body card-date">
-                        <p class="date"><i class="fas fa-calendar-alt"></i>${data.date}</p>
-                        <p class="community"><i class="fas fa-user-alt"></i>by </p>
-                    </div>
-                    </div>
-                    
-                    <div class="card" style="width: 500px;">
-                            <div class="card-body ">
-                                <h5 class="card-title ">${data.info.heading}</h5>
-                            
-                                <p class="card-text">${data.info.description}</p>
-                                
-                    </div>
-         </div>
-         </div>
-       
-       <div id = sale-div>
-         <div class= "big-sale">
-              <button type="button" class="btn btn-success"><em>New to TechKaro? <em>Learn More ></em></em></button>
-            <div class="card" style="width: 18rem;">
-               <div class="card-body">
-                 <h5 class="card-title">${data.sale.heading}</h5>
-                 <p class="card-text">${data.sale.description}</p>
-               </div> 
-           </div>
-        </div>
-        </div> `
-        document.querySelector(".nav1").innerHTML = data.menuOption[0];
-               
-               
-     }
-    
-
-
-
-    });
-}
-function getZh(){
-    $.ajax({
-        url:`https://techkaro-test.herokuapp.com/api/v1/getdata?lang=zh`,
-        success: function(data){
-            console.log(data);
-          
-            
-         document.querySelector("#container").innerHTML = 
-               `  <div id = "main-div">
-                        <h2>A Flower Day...</h2>
-                            <button type="button" class="btn btn-success"><em>what's new at TechKaro?</em></button>
-                        <div class="card" style="width: 500px" "height: 400px" >
-                        <img class="card-img-top" src="${data.imageURL}" alt="Card image cap">
-                        <div class="card-body card-date">
-                            <p class="date"><i class="fas fa-calendar-alt"></i>${data.date}</p>
-                            <p class="community"><i class="fas fa-user-alt"></i>by </p>
-                        </div>
-                        </div>
-                        
-                        <div class="card" style="width: 500px;">
-                                <div class="card-body ">
-                                    <h5 class="card-title ">${data.info.heading}</h5>
-                                
-                                    <p class="card-text">${data.info.description}</p>
-                                    
-                        </div>
-             </div>
-             </div>
-           
-           <div id = sale-div>
-             <div class= "big-sale">
-                  <button type="button" class="btn btn-success"><em>New to TechKaro? <em>Learn More ></em></em></button>
-                <div class="card" style="width: 18rem;">
-                   <div class="card-body">
-                     <h5 class="card-title">${data.sale.heading}</h5>
-                     <p class="card-text">${data.sale.description}</p>
-                   </div> 
-               </div>
-            </div>
-            </div> `
-    
-                   
-                   
-         }
-        
- 
-    
-    });
-}
-
-function getEn(){
-    $.ajax({
-        url:`https://techkaro-test.herokuapp.com/api/v1/getdata?lang=en`,
-
-        success: function(data){
-            console.log(data);
-          
-            
-         document.querySelector("#container").innerHTML = 
-               `  <div id = "main-div">
-                        <h2>A Flower Day...</h2>
-                            <button type="button" class="btn btn-success"><em>what's new at TechKaro?</em></button>
-                        <div class="card" style="width: 500px" "height: 400px" >
-                        <img class="card-img-top" src="${data.imageURL}" alt="Card image cap">
-                        <div class="card-body card-date">
-                            <p class="date"><i class="fas fa-calendar-alt"></i>${data.date}</p>
-                            <p class="community"><i class="fas fa-user-alt"></i>by </p>
-                        </div>
-                        </div>
-                        
-                        <div class="card" style="width: 500px;">
-                                <div class="card-body ">
-                                    <h5 class="card-title ">${data.info.heading}</h5>
-                                
-                                    <p class="card-text">${data.info.description}</p>
-                                    
-                        </div>
-             </div>
-             </div>
-           
-           <div id = sale-div>
-             <div class= "big-sale">
-                  <button type="button" class="btn btn-success"><em>New to TechKaro? <em>Learn More ></em></em></button>
-                <div class="card" style="width: 18rem;">
-                   <div class="card-body">
-                     <h5 class="card-title">${data.sale.heading}</h5>
-                     <p class="card-text">${data.sale.description}</p>
-                   </div> 
-               </div>
-            </div>
-         </div> `
-         document.querySelector(".nav1").innerHTML = data.menuOption[0]; 
-        }
-    });
-}
-
-
-
-
-
-
-
-
